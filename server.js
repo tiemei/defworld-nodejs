05657e6f2a3f875fafbb38b312604ff81f1c9e8a
tiemei <jiadongkai@gmail.com>
2013-05-18 15:10:23 +0800

    add callback

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/tools/flickrAuth', function(req, res){
 });
 
 app.get('/tools/flickrCb', function(req, res) {
-  res.send('hello world');  
+  console.log(req.originalUrl);
+  var oauth_token = req.query.oauth_token
+  var oauth_verifier = req.query.oauth_verifier
+  res.set('Content-Type', 'text/html');
+  res.send('<h3>flickr auth sucess!</h3><br />' + "oauth_token=" + oauth_token +
+           '<br />oauth_verifier=' + oauth_verifier);  
 });
 app.listen(3000);
